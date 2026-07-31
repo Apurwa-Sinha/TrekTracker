dae4a57e3e8376f28828d002242a9a144a7290e4
Apurwa-Sinha <155532263+Apurwa-Sinha@users.noreply.github.com>
2026-07-31 11:11:15 +0530

    Enhance simpleDemonstration for wall node setup
    Refactor simpleDemonstration function to handle center and surrounding nodes more clearly.

--- _simpleDemonstration.js
+++ _simpleDemonstration.js
@@ -1,16 +1,31 @@
 function simpleDemonstration(board) {
   let currentIdY = board.width - 10;
-  for (let counter = 0; counter < 7; counter++) {
-    let currentIdXOne = Math.floor(board.height / 2) - counter;
-    let currentIdXTwo = Math.floor(board.height / 2) + counter;
+  let centerY = Math.floor(board.height / 2);
+  
+  // 1. Do the center node once
+  let centerId = `${centerY}-${currentIdY}`;
+  let centerElement = document.getElementById(centerId);
+  board.wallsToAnimate.push(centerElement);
+  board.nodes[centerId].status = "wall";
+  board.nodes[centerId].weight = 0;
+
+  // 2. Loop from 1 to 6 to do the top and bottom nodes
+  for (let counter = 1; counter < 7; counter++) {
+    let currentIdXOne = centerY - counter;
+    let currentIdXTwo = centerY + counter;
+    
     let currentIdOne = `${currentIdXOne}-${currentIdY}`;
     let currentIdTwo = `${currentIdXTwo}-${currentIdY}`;
+    
     let currentElementOne = document.getElementById(currentIdOne);
     let currentElementTwo = document.getElementById(currentIdTwo);
+    
     board.wallsToAnimate.push(currentElementOne);
     board.wallsToAnimate.push(currentElementTwo);
+    
     let currentNodeOne = board.nodes[currentIdOne];
     let currentNodeTwo = board.nodes[currentIdTwo];
+    
     currentNodeOne.status = "wall";
     currentNodeOne.weight = 0;
     currentNodeTwo.status = "wall";
